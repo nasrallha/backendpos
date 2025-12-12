daa551b1bc16edf5ce222170650c9b5aa956f59e
Mohamed Nasrallah <nasrallhanasrallha85@gmail.com>
2025-12-13 01:25:30 +0300

    update 7

--- config/connectDB.js
+++ config/connectDB.js
@@ -3,16 +3,17 @@ const mongoose = require("mongoose");
 //connect mongoose
 //mongodb://127.0.0.1:27017/infoStore
 // const DB_URI = "mongodb://info:info123456@127.0.0.1:27017/infoStore?authSource=infoStore";
-const connectionDB = async ()=>{
-    try {
-         mongoose.connect(process.env.DB_URL, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
+const connectionDB = async () => {
+  try {
+    const conn = await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
     });
-     console.log('Connected to MongoDB');
-    } catch (error) {
-        console.error('Error connecting to MongoDB:', error);
-    }
+    console.log(`MongoDB connected: ${conn.connection.host}`);
+  } catch (error) {
+    console.error(`MongoDB connection error: ${error.message}`);
+    throw error;
+  }
 };
 
 
